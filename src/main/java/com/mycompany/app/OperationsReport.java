cb1d96fc9dccfbbe3e41f972efa7f3c79f6cdb44
Kemikals <32999864+Kemikals@users.noreply.github.com>
2018-06-25 23:49:12 -1000

    First commit

---
+++ src/main/java/com/mycompany/app/OperationsReport.java
@@ -0,0 +1,160 @@
+package com.mycompany.app;
+
+import java.io.File;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+import org.apache.poi.EncryptedDocumentException;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+public class OperationsReport {
+  Workbook wb;
+
+  private static final int VEHICLE_COUNT_OFFSET =
+      5; // these values are offsets from the cell containing "For specified date range"
+  private static final int NET_SALES_OFFSET = 9;
+  private static final int LABOR_HOURS_OFFSET = 1;
+  private static final int LABOR_DOLLARS_OFFSET = 2;
+
+  private Cell netSalesCell;
+  private Cell vehicleCountCell;
+  private Cell laborHoursCell;
+  private Cell laborDollarsCell;
+
+  private double netSales;
+  private double vehicleCount;
+  private double laborPerVehicle;
+  private double ticketAverage;
+  private double laborNetSales;
+  private double laborHours;
+  private double laborDollars;
+
+  private int startRow; // starting row number for cell containing "for specified date range"
+  private int startCol; // starting column number for cell containing "For specified date range"
+
+  public OperationsReport(String filePath)
+      throws InvalidFormatException, EncryptedDocumentException, IOException {
+    wb = WorkbookFactory.create(new File(filePath));
+
+    extractReportValues(wb); // extracts minimum amount of data to do furthur calculations
+    setReportValues();
+  }
+
+  public void extractReportValues(
+      Workbook wb) { // extracts mimimum amount of data to do furthur calculations.
+
+    if (setStartRowAndCol(wb)) {
+      netSalesCell = getCell(startCol, startRow + NET_SALES_OFFSET);
+      vehicleCountCell = getCell(startCol, startRow + VEHICLE_COUNT_OFFSET);
+      laborHoursCell = getCell(startCol, startRow + LABOR_HOURS_OFFSET);
+      laborDollarsCell = getCell(startCol, startRow + LABOR_DOLLARS_OFFSET);
+
+    } else {
+      System.out.print("Unable to run, couldn't find required cell to generate values");
+    }
+  }
+
+  public void setReportValues() { // sets all values required by either grabbing directly from excel
+    // document, or doing calculation
+    netSales = getCellValue(netSalesCell);
+    vehicleCount = getCellValue(vehicleCountCell);
+    laborHours = getCellValue(laborHoursCell);
+    laborDollars = getCellValue(laborDollarsCell);
+    ticketAverage = calculateTicketAverage();
+    laborPerVehicle = calculateLaborPerVehicle();
+    laborNetSales = calculateLaborNetSales();
+  }
+
+  public double getNetSales() {
+    return netSales;
+  }
+
+  public double getVehicleCount() {
+    return vehicleCount;
+  }
+
+  public double getLaborPerVehicle() {
+    return laborPerVehicle;
+  }
+
+  public double getTicketAverage() {
+    return ticketAverage;
+  }
+
+  public double getLaborNetSales() {
+    return laborNetSales;
+  }
+
+  public double getLaborHours() {
+    return laborHours;
+  }
+
+  public double getLaborDollars() {
+    return laborDollars;
+  }
+
+  public double calculateTicketAverage() {
+
+    return netSales / vehicleCount;
+  }
+
+  private double calculateLaborNetSales() {
+    return laborDollars / netSales;
+  }
+
+  public double calculateLaborPerVehicle() { // returns value to 2 decimal places
+    double value = laborHours / vehicleCount;
+    BigDecimal bd = new BigDecimal(value).setScale(2, RoundingMode.HALF_EVEN);
+    return bd.doubleValue();
+  }
+
+  public double getCellValue(Cell cell) { // returns double value for a cell.
+    return cell.getNumericCellValue();
+  }
+
+  public Cell getCell(
+      int columnNumber, int rowNumber) { // returns a cell at a specified row and column.
+    int sheetNumber = 0;
+    Sheet sheet = wb.getSheetAt(sheetNumber);
+    Row row = sheet.getRow(rowNumber);
+    Cell cell = row.getCell(columnNumber);
+    return cell;
+  }
+
+  public boolean setStartRowAndCol(
+      Workbook wb) { // sets values for startRow and startCol and returns a boolean if cell is found
+    Sheet sheet = wb.getSheetAt(0);
+    DataFormatter dataFormatter = new DataFormatter();
+
+    for (Row row : sheet) {
+      for (Cell cell : row) {
+        if (dataFormatter.formatCellValue(cell).equals("For Specified Date Range")) {
+          startRow = cell.getRowIndex();
+          startCol = cell.getColumnIndex();
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  public Cell getNetSalesCell() {
+    return netSalesCell;
+  }
+
+  public Cell getVehicleCountCell() {
+    return vehicleCountCell;
+  }
+
+  public Cell getLaborDollarsCell() {
+    return laborDollarsCell;
+  }
+}
---
+++ src/main/java/com/mycompany/app/ReportGenerator.java
@@ -0,0 +1,108 @@
+package com.mycompany.app;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.temporal.ChronoField;
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Cell;
+
+public class ReportGenerator {
+  private static final String OPERATIONS_REPORT_FILE_PATH = "Reports/OperationsReport.xls";
+  private static final String EMAIL_STATS_FILE_PATH = "EmailStats.xls";
+
+  private final LocalTime currentTime = LocalTime.now();
+  private final LocalTime sixPM = LocalTime.of(18, 00);
+  private final LocalTime fivePM = LocalTime.of(17, 00);
+  private final LocalTime fourPM = LocalTime.of(16, 00);
+  private final LocalTime twoPM = LocalTime.of(14, 00);
+  private final LocalTime noon = LocalTime.of(12, 00);
+  private final int vehicleCount = 1;
+  private final int netSales = 2;
+  private final int ticketAverage = 3;
+  private final int laborPerVehicle = 5;
+  private final int laborNetSales = 8;
+
+  public void run() {
+    try (FileInputStream emailStats = new FileInputStream(new File(EMAIL_STATS_FILE_PATH))) {
+
+      updateHourlyTracker(emailStats);
+
+    } catch (EncryptedDocumentException e) {
+      System.out.println("File is encrypted");
+    } catch (InvalidFormatException e) {
+      System.out.println("Invalid file type, xls only");
+    } catch (IOException e) {
+      System.out.println("file not found or is in use");
+      e.printStackTrace();
+    }
+  }
+
+  public void updateHourlyTracker(FileInputStream emailStats)
+      throws EncryptedDocumentException, InvalidFormatException, IOException {
+
+    OperationsReport report = new OperationsReport(OPERATIONS_REPORT_FILE_PATH);
+
+    HSSFWorkbook wb = new HSSFWorkbook(emailStats);
+    HSSFSheet worksheet = wb.getSheetAt(0);
+    Cell cell = null;
+
+    updateAllRowValues(report, worksheet, cell);
+    emailStats.close();
+    writeToWorkbook(wb, new FileOutputStream(new File(EMAIL_STATS_FILE_PATH)));
+  }
+
+  public void updateCell(Cell cell, double value) {
+    cell.setCellValue(value);
+  }
+
+  public void updateAllRowValues(OperationsReport report, HSSFSheet worksheet, Cell cell) {
+
+    if (getRowForDayAndTime() != -1) {
+      cell = worksheet.getRow(getRowForDayAndTime()).getCell(vehicleCount);
+      updateCell(cell, report.getVehicleCount());
+      cell = worksheet.getRow(getRowForDayAndTime()).getCell(netSales);
+      updateCell(cell, report.getNetSales());
+      cell = worksheet.getRow(getRowForDayAndTime()).getCell(ticketAverage);
+      updateCell(cell, report.getTicketAverage());
+      cell = worksheet.getRow(getRowForDayAndTime()).getCell(laborPerVehicle);
+      updateCell(cell, report.getLaborPerVehicle());
+      cell = worksheet.getRow(getRowForDayAndTime()).getCell(laborNetSales);
+      updateCell(cell, report.getLaborNetSales());
+    } else {
+      System.out.println("Too early to run report");
+    }
+  }
+
+  public void writeToWorkbook(HSSFWorkbook wb, FileOutputStream outputFile) throws IOException {
+    wb.write(outputFile);
+    outputFile.close();
+  }
+
+  public int getRowForDayAndTime() {
+    DayOfWeek dayOfWeek = DayOfWeek.of(LocalDate.now().get(ChronoField.DAY_OF_WEEK));
+
+    if (dayOfWeek.equals(DayOfWeek.SATURDAY) && currentTime.isAfter(fivePM)) {
+      return 13;
+    } else if (dayOfWeek.equals(DayOfWeek.SUNDAY) && currentTime.isAfter(fourPM)) {
+      return 13;
+    } else if (currentTime.isAfter(sixPM)) {
+      return 13;
+    } else if (currentTime.isAfter(fourPM)) {
+      return 9;
+    } else if (currentTime.isAfter(twoPM)) {
+      return 7;
+    } else if (currentTime.isAfter(noon)) {
+      return 5;
+    } else {
+      return -1;
+    }
+  }
+}
